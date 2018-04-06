2234a72a648c35d67f7972da51c8c10ca5359f15
Zak Hammerman <zhamme2@uic.edu>
2018-04-06 13:18:04 -0500

    Toying around with frontend, React components

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function terminal(props){
+  return <div className="terminal">props.response</div>;
+}
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      status: "disconnected",
+      tty: null
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title"> Welcome to PieShell.</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <terminal response="Ready to begin? Select a TTY instance to get started." />
       </div>
     );
   }
